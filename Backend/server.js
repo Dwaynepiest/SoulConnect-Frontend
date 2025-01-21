9449ff9f673220c832e9c80968d93e7836fbed48
Dwaynepiest <dwaynepiest@outlook.com>
2025-01-21 14:03:25 +0100

    api-key

--- Backend/server.js
+++ Backend/server.js
@@ -4,27 +4,54 @@ const bcrypt = require('bcrypt');
 const db = require('./db'); // Import the database connection
 const port = 3001;
 require('dotenv').config();
-import "routes.js";
-  // Create an Express app
+require("./routes")
+
+const apiKeyMiddleware = (req, res, next) => {
+  const apiKey = req.headers['api-key']; // API key is sent in the 'x-api-key' header
+
+  if (!apiKey || apiKey !== process.env.API_KEY) {
+      return res.status(403).send('Forbidden: Invalid API Key');
+  }
+  next(); // Proceed to the next middleware/route handler
+};
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // Specifieke origin
+    credentials: true 
+  }
+// Create an Express app
 const app = express();
 app.use(cors(corsOptions)); // To allow cross-origin requests
 app.use(express.json()); // To parse JSON bodies
 
 
+app.get('/users', apiKeyMiddleware, (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+      if (err) return res.status(500).send(err);
+      res.json(results);
+  });
+});
 
+app.get('/extra', apiKeyMiddleware, (req, res) => {
+  db.query('SELECT * FROM extra', (err, results) => {
+      if (err) return res.status(500).send(err);
+      res.json(results);
+  });
+});
 
+app.get('/admin', apiKeyMiddleware, (req, res) => {
+  db.query('SELECT * FROM admin', (err, results) => {
+      if (err) return res.status(500).send(err);
+      res.json(results);
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-require("./routes.js")(app);
+app.get('/relatieschap', apiKeyMiddleware, (req, res) => {
+  db.query('SELECT * FROM relatieschap', (err, results) => {
+      if (err) return res.status(500).send(err);
+      res.json(results);
+  });
+});
 
 // Start the serverx
 app.listen(port, () => {
