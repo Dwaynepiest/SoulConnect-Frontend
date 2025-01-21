7dd7e249e703b936b081f160c6bd9fa7489f5f13
TeunPleiter <97097572@st.deltion.nl>
2025-01-21 15:01:17 +0100

    #1.1 Login + Register + Profiel aanmaken (Bijna af)

--- Frontend/soulconnect/src/App.js
+++ Frontend/soulconnect/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Registreren from './pages/Register/Registreren';
+import ProfielAanmaken from './pages/profiel-aanmaken/profiel-aanmaken'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registreren />} />
+        <Route path="/profiel-aanmaken" element={<ProfielAanmaken />} />
+
+      </Routes>
+    </Router>
   );
 }
 
