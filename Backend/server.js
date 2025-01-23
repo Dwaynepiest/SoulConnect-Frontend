422d9f0f1af9265cc0e97f446d36fad0ba7a065f
Dwaynepiest <dwaynepiest@outlook.com>
2025-01-23 09:56:12 +0100

    Posts / puts gemaakt voor gegevens van de account settings

--- Backend/server.js
+++ Backend/server.js
@@ -128,7 +128,7 @@ app.post('/users', apiKeyMiddleware, async (req, res) => {
   }
 });
 
-app.get('/verify-email', (req, res) => {
+app.get('/verify-email', apiKeyMiddleware, (req, res) => {
   const { token } = req.query;
 
   if (!token) {
@@ -157,7 +157,7 @@ app.get('/verify-email', (req, res) => {
 });
 
 
-app.post('/users/login', async (req, res) => {
+app.post('/users/login', apiKeyMiddleware, async (req, res) => {
   const { email, password } = req.body;
 
   db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
@@ -291,6 +291,116 @@ app.get('/extra', apiKeyMiddleware, (req, res) => {
   });
 });
 
+app.post('/extra/:user_id', apiKeyMiddleware, async (req, res) => {
+  const { user_id } = req.params;
+  const { education, hobby, about_you, job } = req.body;
+
+  if (!user_id) {
+    return res.status(400).send('user_id is verplicht.');
+  }
+
+  // Haal bestaande gegevens op voor de opgegeven user_id
+  db.query('SELECT * FROM extra WHERE user_id = ?', [user_id], (err, results) => {
+    if (err) {
+      return res.status(500).send('Databasefout bij het ophalen van gegevens.');
+    }
+
+    if (results.length > 0) {
+      // Update de gegevens als ze al bestaan
+      db.query(
+        'UPDATE extra SET education = ?, hobby = ?, about_you = ?, job = ? WHERE user_id = ?',
+        [
+          education || results[0].education,
+          hobby || results[0].hobby,
+          about_you || results[0].about_you,
+          job || results[0].job,
+          user_id,
+        ],
+        (err) => {
+          if (err) {
+            return res.status(500).send('Fout bij het bijwerken van de gegevens.');
+          }
+
+          res.status(200).json({
+            message: 'Gegevens succesvol bijgewerkt.',
+            user_id,
+            education: education || results[0].education,
+            hobby: hobby || results[0].hobby,
+            about_you: about_you || results[0].about_you,
+            job: job || results[0].job,
+          });
+        }
+      );
+    } else {
+      // Voeg nieuwe gegevens toe als ze nog niet bestaan
+      db.query(
+        'INSERT INTO extra (user_id, education, hobby, about_you, job) VALUES (?, ?, ?, ?, ?)',
+        [user_id, education, hobby, about_you, job],
+        (err, results) => {
+          if (err) {
+            return res.status(500).send('Fout bij het toevoegen van de gegevens.');
+          }
+
+          res.status(201).json({
+            message: 'Gegevens succesvol toegevoegd.',
+            id: results.insertId,
+            user_id,
+            education,
+            hobby,
+            about_you,
+            job,
+          });
+        }
+      );
+    }
+  });
+});
+
+
+app.put('/extra/:user_id', apiKeyMiddleware, async (req, res) => {
+  const { user_id } = req.params;
+  const { education, hobby, about_you, job } = req.body;
+
+  // Haal de bestaande gebruikersgegevens op
+  db.query('SELECT * FROM extra WHERE user_id = ?', [user_id], (err, results) => {
+    if (err) {
+      return res.status(500).send('Databasefout bij het ophalen van gegevens.');
+    }
+    if (results.length === 0) {
+      return res.status(404).send('Geen gegevens gevonden voor deze gebruiker.');
+    }
+
+    const userDetails = results[0];
+
+    // Update alleen de velden die zijn meegegeven in de request body
+    const updatedDetails = {
+      education: education || userDetails.education,
+      hobby: hobby || userDetails.hobby,
+      about_you: about_you || userDetails.about_you,
+      job: job || userDetails.job
+    };
+
+    // Bijwerken van de gegevens in de database
+    db.query(
+      'UPDATE extra SET education = ?, hobby = ?, about_you = ?, job = ? WHERE user_id = ?',
+      [updatedDetails.education, updatedDetails.hobby, updatedDetails.about_you, updatedDetails.job, user_id],
+      (err) => {
+        if (err) {
+          return res.status(500).send('Fout bij het bijwerken van de gegevens.');
+        }
+
+        res.json({
+          message: 'Gegevens succesvol bijgewerkt.',
+          user_id,
+          ...updatedDetails,
+        });
+      }
+    );
+  });
+});
+
+
+
 app.get('/admin', apiKeyMiddleware, (req, res) => {
   db.query('SELECT * FROM admin', (err, results) => {
       if (err) return res.status(500).send(err);
@@ -304,6 +414,72 @@ app.get('/relatieschap', apiKeyMiddleware, (req, res) => {
       res.json(results);
   });
 });
+
+app.post('/relatieschap/:user_id', apiKeyMiddleware, async (req, res) => {
+  const { user_id } = req.params;
+  const { preference, one_liner, relation, location } = req.body;
+
+  if (!user_id) {
+    return res.status(400).send('user_id is verplicht.');
+  }
+
+  // Haal bestaande gegevens op voor de opgegeven user_id
+  db.query('SELECT * FROM relationship WHERE user_id = ?', [user_id], (err, results) => {
+    if (err) {
+      return res.status(500).send('Databasefout bij het ophalen van gegevens.');
+    }
+
+    if (results.length > 0) {
+      // Update de gegevens als ze al bestaan
+      db.query(
+        'UPDATE relationship SET preference = ?, one_liner = ?, relation = ?, location = ? WHERE user_id = ?',
+        [
+          preference || results[0].preference,
+          one_liner || results[0].one_liner,
+          relation || results[0].relation,
+          location || results[0].location,
+          user_id,
+        ],
+        (err) => {
+          if (err) {
+            return res.status(500).send('Fout bij het bijwerken van de gegevens.');
+          }
+
+          res.status(200).json({
+            message: 'Gegevens succesvol bijgewerkt.',
+            user_id,
+            preference: preference || results[0].preference,
+            one_liner: one_liner || results[0].one_liner,
+            relation: relation || results[0].relation,
+            location: location || results[0].location,
+          });
+        }
+      );
+    } else {
+      // Voeg nieuwe gegevens toe als ze nog niet bestaan
+      db.query(
+        'INSERT INTO relationship (user_id, preference, one_liner, relation, location) VALUES (?, ?, ?, ?, ?)',
+        [user_id, preference, one_liner, relation, location],
+        (err, results) => {
+          if (err) {
+            return res.status(500).send('Fout bij het toevoegen van de gegevens.');
+          }
+
+          res.status(201).json({
+            message: 'Gegevens succesvol toegevoegd.',
+            id: results.insertId,
+            user_id,
+            preference,
+            one_liner,
+            relation,
+            location,
+          });
+        }
+      );
+    }
+  });
+});
+
 
 // Start the serverx
 app.listen(port, () => {
