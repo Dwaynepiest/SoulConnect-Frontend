b9b41b5b99daa9e0d1f2ca7941a20bfc5ceb2ccd
TeunPleiter <97097572@st.deltion.nl>
2025-01-24 10:28:25 +0100

    CSS van Register pagina geregeld

--- Frontend/soulconnect/src/App.js
+++ Frontend/soulconnect/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Registreren from './pages/Register/Registreren';
-import ProfielAanmaken from './pages/profiel-aanmaken/profiel-aanmaken';
 import Home from './pages/Home/Home';
 
 
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registreren />} />
-        <Route path="/profiel-aanmaken" element={<ProfielAanmaken />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
